007fae31e8f20dfc19393c71d5e9580f1457f609
Michael Shepard <michael.shepard8@gmail.com>
2020-12-10 15:28:13 -0800

    First commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with Cheese', true);
